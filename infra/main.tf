ccd3eede382aff718da6f6422ded85d241964b69
Faraz Najam <faraz_najam@windows.com>
2025-11-15 23:52:50 +1100

    Add remote backend and key vault module

--- infra/main.tf
+++ infra/main.tf
@@ -1,9 +1,23 @@
 terraform {
+  required_version = ">= 1.6.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.113"
+      version = "~> 4.0"
     }
+  }
+
+  # Backend: Azure Storage (remote state)
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state-file-prod-01"
+    storage_account_name = "terraformstatefileprod01"
+    container_name       = "tfstate"
+    key                  = "keyvault/dev.tfstate"
+
+    # Use Entra ID + OIDC from GitHub
+    use_azuread_auth = true
+    use_oidc         = true
   }
 }
 
@@ -11,40 +25,22 @@ provider "azurerm" {
   features {}
 }
 
-resource "random_string" "random" {
-  length           = 8
-  special          = false
+# Get current tenant / object IDs (from the service principal)
+data "azurerm_client_config" "current" {}
+
+# Example resource group for the Key Vault
+# (You can rename this to whatever convention you like)
+resource "azurerm_resource_group" "rg_kv" {
+  name     = "rg-kv-dev-01"
+  location = "australiaeast"
 }
 
-# Resource Group
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
+# Key Vault module
+module "key_vault" {
+  source = "./modules/key_vault"
+
+  name                = "kv-dev-01"                      # must be globally unique
+  location            = azurerm_resource_group.rg_kv.location
+  resource_group_name = azurerm_resource_group.rg_kv.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
 }
-
-resource "azurerm_service_plan" "asp" {
-  name                = "${var.name_prefix}-${random_string.random.result}"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  os_type             = var.os_type
-  sku_name            = var.sku_name
-}
-
-resource "azurerm_linux_web_app" "app" {
-  name                = var.web_app_name
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  service_plan_id     = azurerm_service_plan.asp.id
-  https_only          = true
-
-  identity { type = "SystemAssigned" }
-
-  site_config {
-    application_stack {
-      dotnet_version = "8.0"
-    }
-  }
-}
-
-output "plan_id"     { value = azurerm_service_plan.asp.id }
-output "webapp_host" { value = azurerm_linux_web_app.app.default_hostname }
